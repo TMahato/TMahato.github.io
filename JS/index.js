a657e685e954ad483d6487cc964a6db8e00bd3e8
TMahato <mahatotanmay99@gmail.com>
2021-07-09 23:10:55 +0530

    upd: Added scroll-down button
    added scroll down button in home page using html,css,js.And added fade
    right to each in hobbies section

--- JS/index.js
+++ JS/index.js
@@ -28,3 +28,11 @@ btnScrollToTop.addEventListener("click",function(){
         behavior: "smooth"
     })
 })
+
+//scroll-down
+$(function () {
+    $('a[href*=#]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
+    });
+});
